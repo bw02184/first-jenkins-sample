d39f80e00debedac1bd3b009653dc9ebb7574915
bw02184 <bw02184@naver.com>
2024-10-08 16:51:55 +0900

    webhook test

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -13,6 +13,6 @@ public class HelloController {
     @GetMapping
     public String sayHello() {
         System.out.println("hello!!");
-        return "Hello, World! 하이";
+        return "Hello, World! 하이22222 test";
     }
 }
